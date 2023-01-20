117ec3e353d8c8cb1e5b05190202687b527c82b1
Alexander Melnikov <melnikov.as.dev@gmail.com>
2023-01-20 20:54:40 +0100

    Clean up OpenAIInteractor

--- lib/domain/open_ai_interactor.dart
+++ lib/domain/open_ai_interactor.dart
@@ -14,52 +14,77 @@ class OpenAIInteractor {
   final OpenAIApiService service;
 
   Future<List<String>> gpt3CleanUpGermanWords(List<String> words) async {
-    final cleanUpPrompt = _getCleanUpWordsPrompt(words);
-    if (cleanUpPrompt.openAITokensLength < maxPromptTokens) {
-      final response = await service.createCompletion(
-        OpenAICompletionRequestBody(
-          prompt: cleanUpPrompt,
-          maxTokens: maxTotalTokens - cleanUpPrompt.openAITokensLength,
-          temperature: 0.1,
-        ),
-      );
-      return _parseResponse([response]);
+    final requestBodies = _generateCompletionRequests(
+      entries: words,
+      promptGenerator: _getCleanUpWordsPrompt,
+    );
+    final responses = await Future.wait(
+        requestBodies.map((e) => service.createCompletion(e)));
+    return _parseCompletionResponses(responses);
+  }
+
+  Future<List<String>> gpt3GenerateFlashcards(List<String> words) async {
+    final requestBodies = _generateCompletionRequests(
+      entries: words,
+      promptGenerator: _generateFlashcardsPrompt,
+    );
+    final responses = await Future.wait(
+        requestBodies.map((e) => service.createCompletion(e)));
+    return _parseCompletionResponses(responses);
+  }
+
+  /// Returns a list of Completion request bodies for a list
+  /// of [entries] and [promptGenerator].
+  ///
+  /// Considers token limits of [maxTotalTokens] and [maxPromptTokens].
+  /// If total [entries] fit the limit, a list with single request is returned.
+  ///
+  /// Otherwise, entries are split between requests, to fit the token limit.
+  List<OpenAICompletionRequestBody> _generateCompletionRequests({
+    required List<String> entries,
+    required String Function(List<String> entries) promptGenerator,
+  }) {
+    final requestBodies = <OpenAICompletionRequestBody>[];
+    final allEntriesPrompt = promptGenerator(entries);
+    if (allEntriesPrompt.openAITokensLength < maxPromptTokens) {
+      requestBodies.add(OpenAICompletionRequestBody(
+        prompt: allEntriesPrompt,
+        maxTokens: maxTotalTokens - allEntriesPrompt.openAITokensLength,
+        temperature: 0.1,
+      ));
     } else {
-      final reqBodies = <OpenAICompletionRequestBody>[];
-      final reqEntries = <String>[];
-      var reqTokens = 0;
-      for (int i = 0; i < words.length; i++) {
-        final nextWord = words[i];
-        if (reqTokens + nextWord.openAITokensLength >= maxPromptTokens) {
-          reqBodies.add(
+      final runEntries = <String>[];
+      var runTokens = 0;
+      for (final entry in entries) {
+        if (runTokens + entry.openAITokensLength >= maxPromptTokens) {
+          requestBodies.add(
             OpenAICompletionRequestBody(
-              prompt: _getCleanUpWordsPrompt(reqEntries),
-              maxTokens: maxTotalTokens - reqTokens,
+              prompt: promptGenerator(runEntries),
+              maxTokens: maxTotalTokens - runTokens,
               temperature: 0.1,
             ),
           );
-          reqEntries.clear();
-          reqTokens = 0;
+          runEntries.clear();
+          runTokens = 0;
         }
-        reqEntries.add(nextWord);
-        reqTokens += nextWord.openAITokensLength;
+        runEntries.add(entry);
+        runTokens += entry.openAITokensLength;
       }
-      if (reqEntries.isNotEmpty) {
-        reqBodies.add(
+      if (runEntries.isNotEmpty) {
+        requestBodies.add(
           OpenAICompletionRequestBody(
-            prompt: _getCleanUpWordsPrompt(reqEntries),
-            maxTokens: maxTotalTokens - reqTokens,
+            prompt: promptGenerator(runEntries),
+            maxTokens: maxTotalTokens - runTokens,
             temperature: 0.1,
           ),
         );
       }
-      final responses =
-          await Future.wait(reqBodies.map((e) => service.createCompletion(e)));
-      return _parseResponse(responses);
     }
+    return requestBodies;
   }
 
-  List<String> _parseResponse(List<OpenAICompletionResponse> responses) {
+  List<String> _parseCompletionResponses(
+      List<OpenAICompletionResponse> responses) {
     final result = <String>[];
     for (final response in responses) {
       if (response.choices.isEmpty) {
@@ -74,7 +99,16 @@ class OpenAIInteractor {
   String _getCleanUpWordsPrompt(List<String> words) {
     return 'Based on a list of german words, create an array with these words in the form: ["word1", "word2", "word3"]. '
         'If the word is a verb, convert it to the infinitive form, if needed. '
-        'If the word is a noun, convert it to the single form and add the definite article. My list of words:\n${words.join('\n')}';
+        'If the word is an adjective, convert it to the single form in nominative case. '
+        'If the word is a noun, convert it to the single form and add the definite article. '
+        'My list of words:\n${words.join('\n')}';
+  }
+
+  String _generateFlashcardsPrompt(List<String> words) {
+    return 'You will generate list of flashcards based on a list of german words. '
+        'For each word, provide one or multiple most popular Russian translations and a short example in german. '
+        'Output in format word|=1=|translation|=2=|example. '
+        'My list of words:\n${words.join('\n')}';
   }
 }
 
